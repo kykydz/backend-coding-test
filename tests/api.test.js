62e40d3893dde5ce89c033bd584efacc32bb1c69
unknown <zkyhersa@gmail.com>
2019-09-23 02:00:31 +0700

    70% code coverage test

--- tests/api.test.js
+++ tests/api.test.js
@@ -9,8 +9,8 @@ const app = require('../src/app')(db);
 const buildSchemas = require('../src/schemas');
 
 describe('API tests', () => {
-    before((done) => {
-        db.serialize((err) => { 
+    before(done => {
+        db.serialize(err => {
             if (err) {
                 return done(err);
             }
@@ -21,12 +21,97 @@ describe('API tests', () => {
         });
     });
 
+    describe('GET /rides', () => {
+        it('should return no rides data', done => {
+            request(app)
+                .get('/rides')
+                .expect('Content-Type', /json/)
+                .expect(res => {
+                    res.body.error_code = 'RIDES_NOT_FOUND_ERROR';
+                })
+                .expect(200, done);
+        });
+    });
+
     describe('GET /health', () => {
-        it('should return health', (done) => {
+        it('should return health', done => {
             request(app)
                 .get('/health')
                 .expect('Content-Type', /text/)
                 .expect(200, done);
+        });
+    });
+
+    describe('POST /rides', () => {
+        describe('# Positive Case', () => {
+            it('should return 200', done => {
+                request(app)
+                    .post('/rides')
+                    .set('content-type', 'application/json')
+                    .send({
+                        start_lat: 80,
+                        end_lat: 80,
+                        start_long: 80,
+                        end_long: 80,
+                        rider_name: 'rider_koko',
+                        driver_name: 'driver_koko',
+                        driver_vehicle: 'vehicle_jet12'
+                    })
+                    .expect(200, done);
+            });
+        });
+
+        describe('# Negative Case', () => {
+            it('should return error_code : VALIDATION_ERROR', done => {
+                request(app)
+                    .post('/rides')
+                    .set('content-type', 'application/json')
+                    .send({
+                        start_lat: -99,
+                        end_lat: 80,
+                        start_long: 80,
+                        end_long: 80,
+                        rider_name: 'rider_koko',
+                        driver_name: 'driver_koko',
+                        driver_vehicle: 'vehicle_jet12'
+                    })
+                    .expect(res => {
+                        res.body.error_code = 'VALIDATION_ERROR';
+                    })
+                    .expect(200, done);
+            });
+        });
+    });
+
+    describe('GET /rides', () => {
+        it('should return list all rides', done => {
+            request(app)
+                .get('/rides')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
+
+    describe('GET /rides/:id', () => {
+        describe('# Positive Case', () => {
+            it('should return data ride with id 1', done => {
+                request(app)
+                    .get('/rides/1')
+                    .expect('Content-Type', /json/)
+                    .expect(200, done);
+            });
+        });
+
+        describe('# Negative Case', () => {
+            it('should return error data not found id 999', done => {
+                request(app)
+                    .get('/rides/999')
+                    .expect('Content-Type', /json/)
+                    .expect(res => {
+                        res.body.error_code = 'RIDES_NOT_FOUND_ERROR';
+                    })
+                    .expect(200, done);
+            });
         });
     });
 });
